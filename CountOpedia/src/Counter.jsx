aa459e9a0b38fb850f112cb321568e81995e14cf
akiman410 <booshkimani@comcast.net>
2026-03-08 17:25:02 -0400

    Final Project Check-in

--- CountOpedia/src/Counter.jsx
+++ CountOpedia/src/Counter.jsx
@@ -1,39 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import attackImg from "./images/attack.png";
 import defenseImg from "./images/defend.png";
 
 function Counter() {
-  const [count, setCount] = useState(0);
-  const [gameStatus, setGameStatus] = useState("");
+  const [gameState, setGameState] = useState({
+    count: 0,
+    gameStatus: "",
+  });
 
   function handleIncrement() {
-    const newValue = count + 1;
-    setCount(newValue);
-    // if (count >= 5) {
-    //   setGameStatus("You Won!");
-    // } else {
-    //   setGameStatus(" ");
-    // }
+    const newCount = gameState.count + 1;
+    const status =
+      newCount >= 5 ? "You Won!!" : newCount <= -5 ? "You Lost!!" : "";
+    setGameState({ count: newCount, gameStatus: status });
   }
+
   function handleDecrement() {
-    const newValue = count - 1;
-    setCount(newValue);
-    // if (count <= -5) {
-    //   setGameStatus("You Lost!");
-    // } else {
-    //   setGameStatus(" ");
-    // }
+    const newCount = gameState.count - 1;
+    const status =
+      newCount <= -5 ? "You Lost!!" : newCount >= 5 ? "You Won!!" : "";
+    setGameState({ count: newCount, gameStatus: status });
   }
-  useEffect(() => {
-    if (count <= -5) {
-      // eslint-disable-next-line react-hooks/set-state-in-effect
-      setGameStatus("You Lost!");
-    } else if (count >= 5) {
-      setGameStatus("You Won!");
-    } else {
-      setGameStatus("");
-    }
-  }, [count]);
 
   function handleRandomPlay() {
     const playMode = Math.round(Math.random());
@@ -44,19 +31,20 @@ function Counter() {
     }
   }
   function handleReset() {
-    setCount(0);
-    setGameStatus("");
+    setGameState({ count: 0, gameStatus: "" });
   }
   function handleLog() {
-    console.log(count);
+    console.log(gameState.count, gameState.gameStatus);
   }
 
   return (
     <div className="container">
       <div className="row text-white text-center">
-        <h1>Game Score : {count}</h1>
+        <h1>Game Score : {gameState.count}</h1>
         <p>You win at +5 points and lose at -5 points</p>
-        {gameStatus.length > 0 && <h3>Game Status :{gameStatus} </h3>}
+        {gameState.gameStatus.length > 0 && (
+          <h3>Game Status :{gameState.gameStatus} </h3>
+        )}
 
         <div className="col-6 col-md-3 offset-md-3">
           <img
